22aa01a84117354234a107b7ab244d39cd76d071
miliasv <milias.vasilis@gmail.com>
2021-10-08 15:31:55 +0000

    minor changes

--- calculate_place_isochrone_intersection.py
+++ calculate_place_isochrone_intersection.py
@@ -8,19 +8,6 @@ from config import params, db_connection_string
 import time
 
 
-def create_poi_ages_table(engine, table_name, metadata):
-    # if table does not exist
-    if not engine.dialect.has_table(engine, table_name):
-        Table(table_name, metadata,
-              Column("fsq_id", String, primary_key=True, nullable=False),
-              Column("INW_014", Numeric),
-              Column("INW_1524", Numeric),
-              Column("INW_2544", Numeric),
-              Column("INW_4564", Numeric),
-              Column("INW_65PL", Numeric))
-        metadata.create_all()
-
-
 def create_place_isochrones_table(engine, table_name, metadata, place_id, schema):
     # if table does not exist
     # if not engine.dialect.has_table(engine, table_name, schema=schema):
@@ -38,10 +25,7 @@ def setup_db(pois_table_name, db_connection, table_type, schema):
     # create object to manage table definitions
     metadata = MetaData(db)
     # create table if it doesn't exist - also define the table.
-    if table_type == "fsq_age":
-        create_poi_ages_table(db, pois_table_name, metadata)
-    else:
-        create_place_isochrones_table(db, pois_table_name, metadata, table_type,schema)
+    create_place_isochrones_table(db, pois_table_name, metadata, table_type,schema)
     # reflect the tables
     Base.prepare(db, schema=schema,reflect=True)
     Table = getattr(Base.classes, pois_table_name)
@@ -87,10 +71,10 @@ def insert_data(session, Table, data):
         session.add(
             Table(**data))
         session.commit()
-        print("~~  INSERTED!")
+        #print("~~  INSERTED!")
     except Exception as err:
         session.rollback()
-        print("# NOT INSERTED: ", err)
+        #print("# NOT INSERTED: ", err)
 
 
 if __name__ == '__main__':
@@ -99,18 +83,20 @@ if __name__ == '__main__':
     # id column name of population data
     pop_id_col = 'c28992r100'
     isochrone_col = "iso_15_avg_speed_75_6"
-    cities = ["eindhoven"]
+    # "eindhoven"
+    cities = ["rotterdam", "utrecht", "hague", "amsterdam"]
     for city_name in cities:
-        pop_table = city_name + "." + city_name[0:3] + '_population_iso_2020_100'
+        print("##### Processing .... #######" + city_name)
+        pop_table = city_name + ".walking_pop_2020_100_" + city_name[0:3]
         # place_table = 'fsq_ams_whole_40_msc_typel1'
-        place_table = city_name + ".pois_" + city_name[0:3]
+        place_table = city_name + ".pois_buurt_" + city_name[0:3]
         id_col = 'osm_id'
         # create table if it doesn't exist
-        to_store_table = 'mapping_pois_' + 'iso_' + city_name[0:3]
+        to_store_table = 'mapping_pois_' + 'walk_' + city_name[0:3]
         session, Table = setup_db(to_store_table, db_connection_string, id_col, city_name)
 
         # get all pois or isochrones
-        max_added_id =  ''
+        max_added_id =  'E0891N4369'
 
         pop_isochrones = get_col_from_db(c, pop_id_col + ',' + isochrone_col, pop_table, pop_id_col, max_added_id)
         added_mappings = get_col_from_db(c, 'id', city_name + "." + to_store_table, 'id', '')
@@ -119,9 +105,9 @@ if __name__ == '__main__':
         for pop_square in pop_isochrones:
             data = {}
             start_time = time.time()
-            print("Calculating --> " + pop_square[pop_id_col])
+            #print("Calculating --> " + pop_square[pop_id_col])
             results = get_place_that_interesect_with_polygon(c, place_table, pop_table, isochrone_col, pop_id_col, pop_square[pop_id_col])
-            print("Time to get query results --- %s seconds ---" % (time.time() - start_time))
+            #print("Time to get query results --- %s seconds ---" % (time.time() - start_time))
             count = 0
             if not results:
                 data["c28992r100"] = pop_square[pop_id_col]
@@ -147,8 +133,8 @@ if __name__ == '__main__':
             # start_time = time.time()
             count += 1
             session.commit()
-            print("~~  INSERTED - " + str(count))
-            print("Time needed  --- %s seconds ---" % (time.time() - start_time))
+            #print("~~  INSERTED - " + str(count))
+            #print("Time needed  --- %s seconds ---" % (time.time() - start_time))
 
 
 
